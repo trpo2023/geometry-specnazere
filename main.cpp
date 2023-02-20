a97cf99819be447871e6e09113036261e8f1f9c6
specnazere <gamer39872987z@mail.ru>
2023-02-20 12:49:02 +0700

    stringsCompare Function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+int stringsCompare(char* firstString, char* secondString)
+{
+	bool isntFinished = true;
+	int i = 0;
+	int difNum = 0;
+	
+	while (isntFinished)
+	{
+		if (firstString[i] != secondString[i] && isntFinished)
+			difNum += 1;
+			
+		if (firstString[i] == '\0' || secondString[i] == '\0')
+			isntFinished == false;
+	}
+	
+	return difNum;
+}
+
 main()
 {
 	cout << "Hello World!";
