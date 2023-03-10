9ea8fd758ad2bbb7d9d2b0d90dd430850800fe03
specnazere <gamer39872987z@mail.ru>
2023-03-10 11:11:08 +0700

    error logging function

--- main.cpp
+++ main.cpp
@@ -19,7 +19,25 @@
 
 using namespace std;
 
-void errorLog(int string, int symbol, int errorCode);
+void errorLog(int string, int symbol, int errorCode)
+{
+	for (int i = 0; i < symbol; i += 1) cout << ' ';
+	cout << '^' << '\n';
+	cout << "line " << string + 1 << ", col " << symbol + 1 << ':';
+	
+	switch(errorCode)
+	{
+		case EMPTY_FIGURE_NAME: cout << " Figure's name must have at least 1 english symbol"; break;
+		case EMPTY_STRING: cout << " Empty line"; break;
+		case EXPECTED_OPENED_BRACKET: cout << " After figure's name data must be in brackets"; break;
+		case INVALID_SYMBOL: cout << " Unexpected symbol"; break;
+		case EXPECTED_FLOAT_TYPE: cout << " there was no point, but requre float type"; break;
+		case EXPECTED_SPACE: cout << " After comma must be space"; break;
+		case EXPECTED_COMMA: cout << " Must be comma among datas"; break;
+		case EXPECTED_CLOSED_BRACKET: cout << " Brackets must be closed"; break;
+	}
+	cout << '\n' << '\n';
+}
 
 bool isDigit(char digit)
 {
